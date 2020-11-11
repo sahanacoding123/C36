168bd8b3f065b2c96a3593fb5ba50bbefa421b89
sahanacoding123 <71457766+sahanacoding123@users.noreply.github.com>
2020-11-11 16:24:38 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas,backgroundImage;
+var gameState=0;
+var playerCount,database,form,player,game;
+
+
+function setup(){
+canvas=createCanvas(400,400);
+database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
+}
+
+function draw(){
+  background("white");
+  
+}
